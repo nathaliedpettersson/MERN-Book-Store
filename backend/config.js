7764445ccc7215b33538ba0b434f301fb1a31c48
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-09-22 17:27:54 +0200

    feat: set up mongoDB and add to project

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBUrl = 'mongodb+srv://root:1234@mern-book-store.dmfpdik.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp';
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from "express";
-import { PORT } from './config.js';
+import { PORT, mongoDBUrl } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('MERN Stack Book Store')
+});
+
+mongoose.connect(mongoDBUrl)
+.then(() => {
+    console.log('App is connected to the database!');
+    app.listen(PORT, () => {
+        console.log(`App is listening to port: ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.log(error, 'Error when trying to connect to database.');
 });
