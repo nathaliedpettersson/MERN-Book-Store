412481a120f61969183fb52e31daf559efa5b796
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-09-26 16:23:56 +0200

    feat: add code for post request and response

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,52 @@
 import express from "express";
 import { PORT, mongoDBUrl } from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+// Parse request body
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('MERN Stack Book Store')
 });
 
-mongoose.connect(mongoDBUrl)
-.then(() => {
-    console.log('App is connected to the database!');
-    app.listen(PORT, () => {
-        console.log(`App is listening to port: ${PORT}`);
-    });
-})
-.catch((error) => {
-    console.log(error, 'Error when trying to connect to database.');
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear'
+            });
+        }
+
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
+
+mongoose.connect(mongoDBUrl)
+    .then(() => {
+        console.log('App is connected to the database!');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error, 'Error when trying to connect to database.');
+    });
