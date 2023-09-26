1e328d833eb6c07b5cc945cce126b572c3980834
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-09-26 16:55:34 +0200

    feat: add code for GET all data in database

--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,22 @@ app.post('/books', async (request, response) => {
     }
 });
 
+// Get all books in database
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+
 mongoose.connect(mongoDBUrl)
     .then(() => {
         console.log('App is connected to the database!');
