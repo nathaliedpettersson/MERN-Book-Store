e149086a74edd4999f8e57ecee914a254eabfd3a
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-09-26 16:56:09 +0200

    feat: add code for getting book by id

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,20 @@ app.get('/books', async (request, response) => {
     }
 });
 
+// Get one specific book from database by using ID
+app.get('/books/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;        
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
 
 mongoose.connect(mongoDBUrl)
     .then(() => {
