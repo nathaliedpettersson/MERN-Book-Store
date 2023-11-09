ef4086ffc47cb1d4af957915dbd2ca405949299b
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-09 21:53:09 +0100

    feat: add logic for creating new book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import CreateBook from './pages/CreateBooks';
+import CreateBook from './pages/CreateBook';
 import ShowBook from './pages/ShowBook';
 import EditBook from './pages/EditBook';
 import DeleteBook from './pages/DeleteBook';
