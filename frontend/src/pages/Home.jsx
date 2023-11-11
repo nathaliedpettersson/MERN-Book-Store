ddbe29f7dfe8bb67ac7b3175870b5885cd1522ef
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-11 20:47:16 +0100

    bug: book.id was missing an _ in Home.jsx to work properly

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
                     </thead>
                     <tbody>
                         {books.map((book, index) => (
-                            <tr key={book.id} className='h-8'>
+                            <tr key={book._id} className='h-8'>
                                 <td className='border border-slate-700 rounded-md text-center'>
                                     {index + 1}
                                 </td>
@@ -62,10 +62,10 @@ const Home = () => {
                                         <Link to={`/books/details/${book._id}`}>
                                             <BsInfoCircle className='text-2xl text-green-800' />
                                         </Link>
-                                        <Link to={`/books/edit/${book.id}`}>
+                                        <Link to={`/books/edit/${book._id}`}>
                                             <AiOutlineEdit className='text-2xl text-yellow-600' />
                                         </Link>
-                                        <Link to={`/books/delete/${book.id}`}>
+                                        <Link to={`/books/delete/${book._id}`}>
                                             <MdOutlineDelete className='text-2xl text-red-600' />
                                         </Link>
                                     </div>
