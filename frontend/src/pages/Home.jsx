5c5e7157b814b7dac82061d273b4b409ad00b997
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-12 22:17:42 +0100

    refactor: small style changes for icons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { BiEdit } from 'react-icons/bi';
+import { BsInfoCircleFill } from 'react-icons/bs';
+import { MdOutlineDelete } from 'react-icons/md';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
 
 const Home = () => {
     const [books, setBooks] = useState([]);
@@ -28,7 +29,7 @@ const Home = () => {
             <div className='flex justify-between items-center'>
                 <h1 className='text3xl my-8'>Books List</h1>
                 <Link to='/books/create'>
-                    <MdOutlineAddBox className='text-sky-800 text-4xl' />
+                    <BsFillPlusCircleFill className='text-sky-800 text-4xl' />
                 </Link>
             </div>
             {loading ? (
@@ -60,10 +61,10 @@ const Home = () => {
                                 <td className='border border-slate-700 rounded-md text-center'>
                                     <div className='flex justify-center gap-x-4'>
                                         <Link to={`/books/details/${book._id}`}>
-                                            <BsInfoCircle className='text-2xl text-green-800' />
+                                            <BsInfoCircleFill className='text-2xl text-green-800' />
                                         </Link>
                                         <Link to={`/books/edit/${book._id}`}>
-                                            <AiOutlineEdit className='text-2xl text-yellow-600' />
+                                            <BiEdit className='text-2xl text-yellow-600' />
                                         </Link>
                                         <Link to={`/books/delete/${book._id}`}>
                                             <MdOutlineDelete className='text-2xl text-red-600' />
