b5aa08a1c08f593e6d18c5756ad7c4815b76868f
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-15 12:23:39 +0100

    refactor: change styling for DeleteBook page

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -25,13 +25,13 @@ const DeleteBook = () => {
     }
 
     return (
-        <div className='p4'>
+        <div className='p4 m-2'>
             <BackButton />
-            <h1 className='text-3xl my-4'>Delete Book</h1>
+            <h1 className='text-3xl text-center my-4'>Delete Book</h1>
             {loading ? <Spinner /> : ''}
-            <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
-                <h3 className='text-2xl'>Are you sure you want to delete this book?</h3>
-                <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>
+            <div className='flex flex-col items-center border-2 border-custom-snow rounded-xl w-[600px] p-8 mx-auto'>
+                <h3 className='text-2xl text-custom-snow'>Are you sure you want to delete this book?</h3>
+                <button className='p-4 bg-red-600 text-white rounded-lg m-8 w-full' onClick={handleDeleteBook}>
                     Yes!
                 </button>
             </div>
