6db481a1f881549cf24d01961ebfd527144b92f9
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2023-11-15 14:46:10 +0100

    refactor: change styling for CreateBook page

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -34,37 +34,37 @@ const CreateBook = () => {
     return (
         <div className='p-4'>
             <BackButton />
-            <h1 className='text-3xl my-4'>Create Book</h1>
+            <h1 className='text-3xl text-custom-snow text-center my-4'>Create Book</h1>
             {loading ? <Spinner /> : ''}
-            <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+            <div className='flex flex-col border-2 border-custom-snow rounded-xl w-[600px] p-4 mx-auto'>
                 <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Title</label>
-                    <input 
-                    type="text"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    className='border-2 border-gray-500 px-4 py-2 w-full'
+                    <label className='text-xl mr-4 text-custom-orange'>Title</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className='rounded-lg px-4 py-2 w-full'
                     />
                 </div>
                 <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Author</label>
-                    <input 
-                    type="text"
-                    value={author}
-                    onChange={(e) => setAuthor(e.target.value)}
-                    className='border-2 border-gray-500 px-4 py-2 w-full'
+                    <label className='text-xl mr-4 text-custom-orange'>Author</label>
+                    <input
+                        type="text"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                        className='rounded-lg px-4 py-2 w-full'
                     />
                 </div>
                 <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
-                    <input 
-                    type="text"
-                    value={publishYear}
-                    onChange={(e) => setPublishYear(e.target.value)}
-                    className='border-2 border-gray-500 px-4 py-2 w-full'
+                    <label className='text-xl mr-4 text-custom-orange'>Publish Year</label>
+                    <input
+                        type="text"
+                        value={publishYear}
+                        onChange={(e) => setPublishYear(e.target.value)}
+                        className='rounded-lg px-4 py-2 w-full'
                     />
                 </div>
-                <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>Add book</button>
+                <button className='p-2 rounded-lg bg-custom-grey text-custom-snow uppercase' onClick={handleSaveBook}>Add book</button>
             </div>
         </div>
     )
